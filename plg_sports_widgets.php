593366ec3cef0c1f88e763ceee63a968726d9e34
DragonZX <DragonZX@yandex.ru>
2020-07-14 22:09:11 +0300

    Delete extra strings

--- plg_sports_widgets.php
+++ plg_sports_widgets.php
@@ -1,19 +1,10 @@
 <?php
-/**
-* @ 2013 Benj Golding. All rights reserved.
-* @GNU/GPL licence
-*
-*/
 
 // Assert file included in Joomla!
 defined( '_JEXEC' ) or die( 'Restricted access' );
 
 jimport( 'joomla.plugin.plugin' );
 
-/**
-* SportsWidgets Content Plugin
-*
-*/
 class plgContentSWPlugin extends JPlugin
 {
 	function PluginSW( &$subject, $params )
@@ -25,22 +16,13 @@ class plgContentSWPlugin extends JPlugin
 		{
 		global $mainframe;
 		
-
-		if ( JString::strpos( $article->text, '{geniussports}' ) === false ) {
-		return true;
-		}
-		
+		if ( JString::strpos( $article->text, '{geniussports}' ) === false ) {return true;}
 		$article->text = preg_replace_callback('|{geniussports}(.*){\/geniussports}|',function ($match){return $this->embedWidget($match[1]);}, $article->text);
-		
-			
-
 		return true;
-	
 	}
 
 	function embedWidget($vCode)
 	{
 	    return "<!-- Genius Sports Widget --><div id = '".$vCode."'></div><script>window.".$vCode." = {}; (function(a,b,c,d,e,f){e=b.createElement('script');f=b.getElementsByTagName('script')[0];e.async=1;e.src=c+'/?'+d;f.parentNode.insertBefore(e,f)})(window,document,'https://widget.wh.geniussports.com/widget/','".$vCode."');</script>";
 	}
-
 }
